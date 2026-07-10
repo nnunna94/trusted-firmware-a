98cd26e98e3f89b047621a75436fe2379e92b3a4
Naresh Nunna <nnunna@qti.qualcomm.com>
2026-07-10 13:53:06 +0530

    feat(qti): enable chipinfo driver
    Wire the ChipInfo driver into the QTI platforms and switch the SoC
    version lookup over to it.
    
    - qti_chipinfo_init() is called from bl31_platform_setup() (recoverable:
      a failure logs a WARN and continues, since the only consumer today is
      informational).
    - plat_get_soc_version() now derives the SoC version from
      chipinfo_get_chip_id() instead of the static JTAG-ID-to-chipinfo map.
    - The now-unused per-platform qti_map_chipinfo.h tables and the
      QTI_DEFAULT_CHIPINFO_ID / JTAG map machinery are removed.
    - chipinfo.mk is included from each QTI platform makefile. For lemans_evk
      it is included alongside smem.mk, outside the QTISECLIB_PATH conditional,
      because plat_get_soc_version() consumes chipinfo in both the stub and the
      real-qtiseclib configs.
    
    Build-tested for PLAT=lemans_evk (bl31).
    
    Change-Id: I7a2f4c9e8b1d3f60a5c4e2b7d9f81a3c6e5b0d24
    Co-developed-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Signed-off-by: Naresh Nunna <nnunna@qti.qualcomm.com>

--- plat/qti/common/src/qti_bl31_setup.c
+++ plat/qti/common/src/qti_bl31_setup.c
@@ -14,6 +14,7 @@
 #include <drivers/generic_delay_timer.h>
 #include <drivers/qti/accesscontrol/accesscontrol.h>
 #include <drivers/qti/accesscontrol/xpu.h>
+#include <drivers/qti/chipinfo/chipinfo.h>
 #include <drivers/qti/qtimer/qtimer.h>
 #include <drivers/qti/sec_core/sec_core.h>
 #include <drivers/qti/smmu/smmu.h>
@@ -100,6 +101,9 @@ void bl31_platform_setup(void)
 
 	plat_qti_gic_driver_init();
 	plat_qti_gic_init();
+	if (qti_chipinfo_init() != CHIPINFO_SUCCESS) {
+		WARN("ChipInfo initialization error\n");
+	}
 	qti_smmu_init();
 	qti_interrupt_svc_init(bl32_image_ep_info.pc != 0);
 	qti_sec_core_init();
